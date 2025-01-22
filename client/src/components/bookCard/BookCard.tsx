6f4956c0cd997d46393c4bce75b5f06ce146c9a8
olga-loisha <olga.lojsza@gmail.com>
2025-01-22 14:55:57 +0100

    Add images

--- client/src/components/bookCard/BookCard.tsx
+++ client/src/components/bookCard/BookCard.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './BookCard.css';
 import { BookCardProps } from '../../models';
+import { imagesResolver } from '../../utils/imagesResolver';
 
 const BookCard = (props: BookCardProps) => {
   const { book } = props;
 
   return (
       <div className="book-card">
-          <img className="book-card-cover" src={book.url} />
+          <img className="book-card-cover" src={imagesResolver(book.id)} />
           <div className="book-card-info">
               <p>
                   <span className="book-card-name">{book?.name}</span> by <span
---
+++ client/src/utils/imagesResolver.ts
@@ -0,0 +1,29 @@
+export function imagesResolver(id: number): string {
+    switch (id) {
+        case 1: return '/covers/pearl.jpg';
+        case 2: return '/covers/casualvacancy.jpg';
+        case 3: return '/covers/lud.jpg';
+        case 4: return '/covers/dalemark.jpg';
+        case 5: return '/covers/octoberjpg.jpg';
+        case 6: return '/covers/tuckjpg.jpg';
+        case 7: return '/covers/compass.jpg';
+        case 8: return '/covers/addie.jpg';
+        case 9: return '/covers/poppy.jpg';
+        case 10: return '/covers/piranesi.jpg';
+        case 11: return '/covers/possession.jpg';
+        case 12: return '/covers/tress.jpg';
+        case 13: return '/covers/fairies.jpg';
+        case 14: return '/covers/queene.jpg';
+        case 15: return '/covers/magicians.jpg';
+        case 16: return '/covers/garden.jpg';
+        case 17: return '/covers/instant.jpg';
+        case 18: return '/covers/wolves.jpg';
+        case 19: return '/covers/cityofgirls.jpg';
+        case 20: return '/covers/odyssey.jpg';
+        case 21: return '/covers/godkiller.jpg';
+        case 22: return '/covers/april.jpg';
+        case 23: return '/covers/bittersweet.jpg';
+        case 24: return '/covers/dragons.jpg';
+        default: return '';
+    }
+}
